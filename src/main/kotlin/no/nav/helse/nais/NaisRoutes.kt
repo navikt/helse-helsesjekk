3b288c37279eb2752fc0e0ba9a09ad7cab436066
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2019-01-24 11:36:19 +0100

    Log http requests

--- src/main/kotlin/no/nav/helse/nais/NaisRoutes.kt
+++ src/main/kotlin/no/nav/helse/nais/NaisRoutes.kt
@@ -1,23 +1,30 @@
 package no.nav.helse.nais
 
-import io.ktor.application.*
-import io.ktor.http.*
-import io.ktor.response.*
-import io.ktor.routing.*
-import io.prometheus.client.*
-import io.prometheus.client.exporter.common.*
-import java.util.*
+import io.ktor.application.application
+import io.ktor.application.call
+import io.ktor.application.log
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.response.respondTextWriter
+import io.ktor.routing.Routing
+import io.ktor.routing.get
+import io.prometheus.client.CollectorRegistry
+import io.prometheus.client.exporter.common.TextFormat
+import java.util.Collections
 
 fun Routing.nais(collectorRegistry: CollectorRegistry) {
     get("/isalive") {
+        application.log.trace("/isalive called")
         call.respondText("ALIVE", ContentType.Text.Plain)
     }
 
     get("/isready") {
+        application.log.trace("/isready called")
         call.respondText("READY", ContentType.Text.Plain)
     }
 
     get("/metrics") {
+        application.log.trace("/metrics called")
         val names = call.request.queryParameters.getAll("name[]")?.toSet() ?: Collections.emptySet()
         call.respondTextWriter(ContentType.parse(TextFormat.CONTENT_TYPE_004)) {
             TextFormat.write004(this, collectorRegistry.filteredMetricFamilySamples(names))
